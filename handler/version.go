3d669a292bf12128707c3e8c34ff98e97841e662
Anton Baklanov <antonbaklanov@gmail.com>
2026-01-01 22:32:17 -0300

    Cleanups version handler

--- handler/version.go
+++ handler/version.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"bytes"
 	"encoding/json"
 	"log/slog"
 	"net/http"
@@ -11,15 +10,13 @@ import (
 
 func Version(w http.ResponseWriter, r *http.Request) {
 	vv := version.GetBuildInfo()
-	buf := new(bytes.Buffer)
-	if err := json.NewEncoder(buf).Encode(vv); err != nil {
-		slog.Error("cannot encode version", "error", err)
+	resp, err := json.Marshal(&vv)
+	if err != nil {
+		slog.Error("cannot marshal version info", "error", err)
 		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		if _, err := w.Write(buf.Bytes()); err != nil {
-			slog.Error("cannot write version", "error", err)
-		}
+		return
 	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
 }
