26b31e992e44f48feb87c36344f58f5d02007f92
Anton Baklanov <antonbaklanov@gmail.com>
2026-03-07 16:06:48 -0300

    Format the code

--- store/mock/mock.go
+++ store/mock/mock.go
@@ -152,12 +152,12 @@ func (m *MockRepoStore) Delete(ctx context.Context, id uint64) error {
 
 // MockArtifactStore implements store.ArtifactStore.
 type MockArtifactStore struct {
-	CreateFn      func(ctx context.Context, artifact *store.Artifact) error
-	GetFn         func(ctx context.Context, id uint64) (*store.Artifact, error)
-	ListFn        func(ctx context.Context) ([]*store.Artifact, error)
-	ListByRepoFn  func(ctx context.Context, repoID uint64) ([]*store.Artifact, error)
-	CountByRepoFn func(ctx context.Context, repoID uint64) (int, error)
-	DeleteFn      func(ctx context.Context, id uint64) error
+	CreateFn       func(ctx context.Context, artifact *store.Artifact) error
+	GetFn          func(ctx context.Context, id uint64) (*store.Artifact, error)
+	ListFn         func(ctx context.Context) ([]*store.Artifact, error)
+	ListByRepoFn   func(ctx context.Context, repoID uint64) ([]*store.Artifact, error)
+	CountByRepoFn  func(ctx context.Context, repoID uint64) (int, error)
+	DeleteFn       func(ctx context.Context, id uint64) error
 	DeleteByRepoFn func(ctx context.Context, repoID uint64) error
 }
 
